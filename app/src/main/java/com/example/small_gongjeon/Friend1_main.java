83ce44080355b84874fc999b43f8a086870e6518
Lee-Sunho <sunho9889@naver.com>
2021-08-04 01:38:49 +0900

    친구 플러스 버튼 팝업창 구현

--- app/src/main/java/com/example/small_gongjeon/Friend1_main.java
+++ app/src/main/java/com/example/small_gongjeon/Friend1_main.java
@@ -1,22 +1,36 @@
 package com.example.small_gongjeon;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class Friend1_main extends Fragment {
+public class Friend1_main extends Fragment implements View.OnClickListener{
     private View view;
+    ImageButton btn_add_friend;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.activity_friend1_main,container,false);
+        btn_add_friend = (ImageButton)view.findViewById(R.id.btn_add_friend);
+        btn_add_friend.setOnClickListener(this);
         return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_add_friend:
+                getActivity().startActivity(new Intent(getActivity(), Friend2_AddFriend.class));
+        }
+
     }
 }
