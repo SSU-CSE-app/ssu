ef4a42c056a4b6aff8e3822ad3d552b8b11ee76d
Lee-Sunho <sunho9889@naver.com>
2021-08-04 01:33:54 +0900

    알람, 그룹 플러스 버튼 팝업창 구현

--- app/src/main/java/com/example/small_gongjeon/Alarm2_PlusAlarm.java
+++ app/src/main/java/com/example/small_gongjeon/Alarm2_PlusAlarm.java
@@ -3,12 +3,31 @@ package com.example.small_gongjeon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.Window;
 
 public class Alarm2_PlusAlarm extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_alarm2_plus_alarm);
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //배경 클릭시 꺼지는거 막기
+        if( event.getAction() == MotionEvent.ACTION_OUTSIDE ) {
+            return false;
+        }
+        return true;
+    }
+
+    /*
+    @Override
+    public void onBackPressed() {
+        //안드로이드 백버튼 막기
+        return;
+    }*/
 }
--- app/src/main/java/com/example/small_gongjeon/Group1_main.java
+++ app/src/main/java/com/example/small_gongjeon/Group1_main.java
@@ -1,22 +1,38 @@
 package com.example.small_gongjeon;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class Group1_main extends Fragment {
+public class Group1_main extends Fragment implements View.OnClickListener{
     private View view;
+    ImageButton btn_add_group;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.activity_group1_main,container,false);
+        btn_add_group = (ImageButton)view.findViewById(R.id.btn_add_group);
+        btn_add_group.setOnClickListener(this);
         return view;
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_add_group:
+                getActivity().startActivity(new Intent(getActivity(), Group2_AddGroup.class));
+        }
+
+    }
+
+
 }
