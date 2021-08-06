7a5ae4ef660226f5552ea0496238209474b70e35
Lee-Sunho <sunho9889@naver.com>
2021-08-07 02:16:11 +0900

    친구창 리스트뷰 하드코딩

--- app/src/main/java/com/example/small_gongjeon/Friend1_main.java
+++ app/src/main/java/com/example/small_gongjeon/Friend1_main.java
@@ -7,9 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 public class Friend1_main extends Fragment implements View.OnClickListener{
@@ -22,6 +24,14 @@ public class Friend1_main extends Fragment implements View.OnClickListener{
         view=inflater.inflate(R.layout.activity_friend1_main,container,false);
         btn_add_friend = (ImageButton)view.findViewById(R.id.btn_add_friend);
         btn_add_friend.setOnClickListener(this);
+        ListView lv = view.findViewById(R.id.listview_friend);
+        FriendList adapter = new FriendList();
+
+        lv.setAdapter(adapter);
+
+        adapter.addFriend(ContextCompat.getDrawable(getActivity(),R.drawable.ic_launcher_background), "이선호", "좀 되라");
+        adapter.addFriend(ContextCompat.getDrawable(getActivity(),R.drawable.tap_friends), "이민지", "하이하이");
+        adapter.addFriend(ContextCompat.getDrawable(getActivity(),R.drawable.tap_friends), "김흥수", "!!!!!!!");
         return view;
     }
 
