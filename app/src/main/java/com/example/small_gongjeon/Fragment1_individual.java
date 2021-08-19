87c7f790d53729514b5500398e22761128acda8e
LeeMinJi <julia7450@naver.com>
2021-08-19 17:12:43 +0900

    개인/그룹알람 추가 탭 구현

---
+++ app/src/main/java/com/example/small_gongjeon/Fragment1_individual.java
@@ -0,0 +1,20 @@
+package com.example.small_gongjeon;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Fragment1_individual extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment1_individual, container, false);
+
+        return view;
+    }
+}
