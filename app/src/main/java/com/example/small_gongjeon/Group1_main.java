ad6083ce5726e319744747da89644166c8f5c629
Lee-Sunho <sunho9889@naver.com>
2021-08-20 22:50:43 +0900

    그룹메인에 그룹 끌고오기, 클릭 이벤트 완성

--- app/src/main/java/com/example/small_gongjeon/Group1_main.java
+++ app/src/main/java/com/example/small_gongjeon/Group1_main.java
@@ -1,8 +1,12 @@
 package com.example.small_gongjeon;
 
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,15 +14,38 @@ import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class Group1_main extends Fragment implements View.OnClickListener{
+    private static String IP_ADDRESS = "27.96.134.147";
+    private static String TAG = "small_gongjeon";
+
+    private ArrayList<Group> mArrayList;
+    private GroupAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+    private String mJsonString;
     private View view;
+    private TextView group_num;
     ImageButton btn_add_group;
 
     @Nullable
@@ -29,23 +56,170 @@ public class Group1_main extends Fragment implements View.OnClickListener{
         btn_add_group.setOnClickListener(this);
 
 
-        ListView lv = view.findViewById(R.id.listview_group);
-        GroupList adapter = new GroupList();
+        group_num = (TextView)view.findViewById(R.id.tv_group_num);
+        mRecyclerView = (RecyclerView)view.findViewById(R.id.recyclerview_group);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        lv.setAdapter(adapter);
+        mArrayList = new ArrayList<>();
+        mAdapter = new GroupAdapter(getActivity(), mArrayList);
 
-        adapter.addGroup(ContextCompat.getDrawable(getActivity(),R.drawable.ic_launcher_background), "뚱이", 7);
-        adapter.addGroup(ContextCompat.getDrawable(getActivity(),R.drawable.tap_friends), "파워레인저", 5);
-        adapter.addGroup(ContextCompat.getDrawable(getActivity(),R.drawable.tap_profile), "소공전", 3);
-
-        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mAdapter.setOnItemClickListener(new GroupAdapter.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                getActivity().startActivity(new Intent(getActivity(), Group3_GroupInfo.class));
+            public void onItemClick(View view, int position) {
+                Context c = view.getContext();
+                Intent intent = new Intent(c,Group3_GroupInfo.class);
+                startActivity(intent);
             }
         });
+        mRecyclerView.setAdapter(mAdapter);
+
+        mArrayList.clear();
+        mAdapter.notifyDataSetChanged();
+
+        String Keyword = Main.userID;
+
+        GetData task = new GetData();
+        task.execute( "http://" + IP_ADDRESS + "/group_info_request.php", Keyword);
+
+
         return view;
     }
+
+    private class GetData extends AsyncTask<String, Void, String> {
+
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(getActivity(),
+                    "Please Wait", null, true, true);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+//            mTextViewResult.setText(result);
+            Log.d(TAG, "response - " + result);
+
+            if (result == null) {
+
+//                mTextViewResult.setText(errorString);
+            } else {
+
+                mJsonString = result;
+                showResult();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String serverURL = params[0];
+            String postParameters = "userId=" + params[1];
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+
+                return sb.toString().trim();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+
+                return null;
+            }
+
+        }
+    }
+
+    private void showResult(){
+
+        String TAG_JSON="webnautes";
+        String TAG_NAME = "group";
+        //String TAG_Message = "userStatus";
+
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+            System.out.println("jsonAraay : "+jsonArray);
+            //System.out.println("jsonLength : "+jsonArray.length());
+            //String temp_group_num = String.valueOf(jsonArray.length());
+            //group_num.setText(temp_group_num);
+            for(int i=0;i<jsonArray.length();i++){
+//                System.out.println("currJson: "+jsonArray.getJSONObject(i));
+                JSONObject item = jsonArray.getJSONObject(i);
+
+                String name = item.getString(TAG_NAME);
+                //String message = item.getString(TAG_Message);
+
+                Group group = new Group();
+
+                group.setName(name);
+
+
+                mArrayList.add(group);
+                mAdapter.notifyDataSetChanged();
+            }
+
+
+
+        } catch (JSONException e) {
+
+            Log.d(TAG, "showResult : ", e);
+        }
+
+    }
+
+
 
     @Override
     public void onClick(View view) {
@@ -55,5 +229,7 @@ public class Group1_main extends Fragment implements View.OnClickListener{
         }
 
     }
+
+
 
 }
---
+++ app/src/main/java/com/example/small_gongjeon/GroupAdapter.java
@@ -0,0 +1,83 @@
+package com.example.small_gongjeon;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class GroupAdapter extends RecyclerView.Adapter<GroupAdapter.CustomViewHolder>{
+    private ArrayList<Group> mList = null;
+    private Activity context = null;
+    private OnItemClickListener mListener = null;
+
+    public interface OnItemClickListener{
+        void onItemClick(View view, int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener){
+        this.mListener = listener;
+    }
+
+    public GroupAdapter(Activity context, ArrayList<Group> list){
+        this.context = context;
+        this.mList = list;
+    }
+
+    class CustomViewHolder extends  RecyclerView.ViewHolder{
+        protected TextView name;
+        protected TextView number;
+        protected ImageView img;
+        protected ImageView img2;
+
+        public CustomViewHolder(View view) {
+            super(view);
+            this.name = (TextView) view.findViewById(R.id.name_group);
+            this.number = (TextView) view.findViewById(R.id.number_group_member);
+            this.img = (ImageView) view.findViewById(R.id.image_group);
+            this.img2 = (ImageView) view.findViewById(R.id.image_number_group_member);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    int position = getAbsoluteAdapterPosition();
+                    if(position != RecyclerView.NO_POSITION){
+                        if(mListener != null){
+                            mListener.onItemClick(view,position);
+                        }
+                    }
+                }
+            });
+
+        }
+    }
+
+    @Override
+    public CustomViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.listviewitem_group,null);
+        CustomViewHolder viewHolder = new CustomViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomViewHolder viewholder, int position) {
+
+        viewholder.name.setText(mList.get(position).getName());
+//        viewholder.number.setText("" + mList.get(position).getNumber());
+//        viewholder.img.setImageDrawable(mList.get(position).getD());
+    }
+    @Override
+    public int getItemCount() {
+        return (null != mList ? mList.size() : 0);
+    }
+}
