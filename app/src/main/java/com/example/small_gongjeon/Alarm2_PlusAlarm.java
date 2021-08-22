77ca4589916a9255ac93f008c99ea167097db5b3
LeeMinJi <julia7450@naver.com>
2021-08-22 15:55:43 +0900

    알람추가-다시알림 스피너 두 개

--- app/src/main/java/com/example/small_gongjeon/Alarm2_PlusAlarm.java
+++ app/src/main/java/com/example/small_gongjeon/Alarm2_PlusAlarm.java
@@ -21,19 +21,31 @@ import static java.security.AccessController.getContext;
 
 public class Alarm2_PlusAlarm extends AppCompatActivity {
     Fragment fragment1,fragment2;
+    Spinner spinner_min;
+    Spinner spinner_num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm2_plus_alarm);
 
+
+        // 개인/그룹 탭 전환
         fragment1 = new Fragment1_individual();
         fragment2 = new Fragment2_group();
 
         getSupportFragmentManager().beginTransaction().add(R.id.frame, fragment1).commit();
-
         TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+/*        // 알람 반복 스피너 (몇분마다, 몇번반복)
+        spinner_min = findViewById(R.id.spinner_alarm_min);
+        spinner_num = findViewById(R.id.spinner_alarm_num);
 
+        ArrayAdapter minAdapter = ArrayAdapter.createFromResource(this,R.array.repeat_alarm_min, android.R.layout.simple_spinner_dropdown_item);
+        spinner_min.setAdapter(minAdapter);
+
+        ArrayAdapter numAdapter = ArrayAdapter.createFromResource(this,R.array.repeat_alarm_num, android.R.layout.simple_spinner_dropdown_item);
+        spinner_num.setAdapter(numAdapter);
+*/
         tabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -42,7 +54,6 @@ public class Alarm2_PlusAlarm extends AppCompatActivity {
 
                 Fragment selected = null;
                 if(position == 0){
-
                     selected = fragment1;
 
                 }
