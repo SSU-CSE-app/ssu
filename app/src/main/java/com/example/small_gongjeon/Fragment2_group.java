6610ea94113adcfb19af4988d931172fb97c92ad
Lee-Sunho <sunho9889@naver.com>
2021-08-22 23:23:21 +0900

    그룹알람 추가탭 그룹 목록 스피너 완성

--- app/src/main/java/com/example/small_gongjeon/Fragment2_group.java
+++ app/src/main/java/com/example/small_gongjeon/Fragment2_group.java
@@ -1,9 +1,13 @@
 package com.example.small_gongjeon;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
@@ -19,18 +23,28 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class Fragment2_group extends Fragment {
     private static String IP_ADDRESS = "27.96.134.147";
     private static String TAG = "small_gongjeon";
 
+    private ArrayList groupList = new ArrayList<>();
+    private String mJsonString;
     private TimePicker time;
     Spinner spinner_min;
     Spinner spinner_num;
+    Spinner spinner_group;
 
     private CheckBox Mon;
     private CheckBox Tue;
@@ -94,13 +108,23 @@ public class Fragment2_group extends Fragment {
         alarmDay = 0;
         isRepeatChecked = "0";
 
+        //그룹 목록
+        String Keyword = Main.userID;
+        groupList.clear();
+        GetData task = new GetData();
+        task.execute( "http://" + IP_ADDRESS + "/group_info_request_final.php", Keyword);
+
+        spinner_group = (Spinner)view.findViewById(R.id.spinner_select_group);
+
+
         // 개인 알람 추가 버튼 누를시 실행
         btn_addAlarm.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 String userId = Main.userID;
                 String alarmTime = time.getCurrentHour().toString()+":"+time.getCurrentMinute().toString();
-                groupName = "group_01"; //TODO 그룹 선택 기능 구현하면 바꾸기
+                //groupName = "group_01"; //TODO 그룹 선택 기능 구현하면 바꾸기
+                groupName = spinner_group.getSelectedItem().toString();
                 // 요일 숫자로 변환
                 if(Mon.isChecked()){
                     alarmDay += 1000000;
@@ -187,5 +211,135 @@ public class Fragment2_group extends Fragment {
         });
 
         return view;
+    }
+    private class GetData extends AsyncTask<String, Void, String> {
+
+        ProgressDialog progressDialog;
+        String errorString = null;
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog = ProgressDialog.show(getActivity(),
+                    "Please Wait", null, true, true);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+//            mTextViewResult.setText(result);
+            Log.d(TAG, "response - " + result);
+
+            if (result == null) {
+
+//                mTextViewResult.setText(errorString);
+            } else {
+
+                mJsonString = result;
+                showResult();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String serverURL = params[0];
+            String postParameters = "userId=" + params[1];
+
+
+            try {
+
+                URL url = new URL(serverURL);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+
+                httpURLConnection.setReadTimeout(5000);
+                httpURLConnection.setConnectTimeout(5000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.connect();
+
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8"));
+                outputStream.flush();
+                outputStream.close();
+
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
+                    inputStream = httpURLConnection.getInputStream();
+                }
+                else{
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line;
+
+                while((line = bufferedReader.readLine()) != null){
+                    sb.append(line);
+                }
+
+                bufferedReader.close();
+
+                return sb.toString().trim();
+
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+                errorString = e.toString();
+
+                return null;
+            }
+
+        }
+    }
+    private void showResult(){
+
+        String TAG_JSON="webnautes";
+        String TAG_NAME = "group";
+
+
+        try {
+            JSONObject jsonObject = new JSONObject(mJsonString);
+            JSONArray jsonArray = jsonObject.getJSONArray(TAG_JSON);
+            System.out.println("jsonAraay : "+jsonArray);
+            System.out.println("jsonLength : "+jsonArray.length());
+            groupList.clear();
+            for(int i=0;i<jsonArray.length();i++){
+//                System.out.println("currJson: "+jsonArray.getJSONObject(i));
+                JSONObject item = jsonArray.getJSONObject(i);
+
+                String name = item.getString(TAG_NAME);
+                //String message = item.getString(TAG_Message);
+
+                groupList.add(name);
+            }
+
+            System.out.println("그룹리스트: " + groupList);
+            ArrayAdapter<String> groupAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_dropdown_item, groupList);
+            spinner_group.setAdapter(groupAdapter);
+            System.out.println("그룹리스트: " + groupList);
+
+
+
+        } catch (JSONException e) {
+
+            Log.d(TAG, "showResult : ", e);
+        }
+
     }
 }
